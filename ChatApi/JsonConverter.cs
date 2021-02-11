944e4bde3a2914ddbd7f95f22ce0810985f00e1d
Peter Cholub <ripiter15@wp.pl>
2021-02-11 09:31:33 +0100

    Added deserialize json

---
+++ ChatApi/JsonConverter.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApi
+{
+    public static class JsonConverter
+    {
+        public static T Deserialize<T>(string _value)
+        {
+            return JsonConvert.DeserializeObject<T>(_value);
+        }
+    }
+}
